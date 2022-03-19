bee6c6af8c7420261962d7b16f7910909cca41cf
Siddharth <officialsiddharthbisht@gmail.com>
2022-03-19 19:58:28 +0530

    component to import navbar

---
+++ Local Gym WebSite/components/navbar.js
@@ -0,0 +1,22 @@
+function navbar(){
+  return ` 
+<!-- left box for logos -->
+<div class="left">
+  <img src="/img/dp.png" alt="" />
+</div>
+<!-- mid box for navbar -->
+<div class="mid">
+  <ul class="navbar">
+    <li><a href="/index.html">Home</a></li>
+    <li><a href="/aboutUs/aboutus.html">About-Us</a></li>
+    <li><a href="#">Fitness-Calculator</a></li>
+    <li><a href="/contactPage/contactus.html">Contact-Us</a></li>
+  </ul>
+</div>
+<!-- right bx for buttons -->
+<div class="right">
+  <button class="btn">Call Us Now</button>
+  <button class="btn">Email Us</button>
+</div>`;
+}
+export default navbar;
