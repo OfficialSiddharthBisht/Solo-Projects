59dc47c8b210a51f3811d15cab0ee8fd5bc13620
Siddharth <officialsiddharthbisht@gmail.com>
2022-03-19 19:58:16 +0530

    component to import footer

---
+++ Local Gym WebSite/components/footer.js
@@ -0,0 +1,32 @@
+function footer(){
+    return `      
+<div class="first-para">
+    <h2>Your Queries</h2>
+    <ul>
+        <li> <a href="#">Fees</a> </li>
+        <li><a href="#">Membership Program</a></li>
+        <li><a href="#">Trainer</a></li>
+        <li><a href="#">Personal Trainer</a></li>
+        <li><a href="#">Work With Us</a></li>
+    </ul>
+</div>
+<div class="second-para">
+    <h2>Why us ?</h2>
+    <ul>
+        <li><a href="/aboutUs/aboutus.html">About Us</a></li>
+        <li><a href="#">Know more about us</a></li>
+        <li><a href="#">Locate Us</a></li>
+    </ul>
+</div>
+<div class="third-para">
+    <h2>Connect Us On Socials</h2>
+    <ul>
+        <li><a href="#" target="_blank">Instagram</a></li>
+        <li><a href="#" target="_blank">Facebook</a></li>
+        <li><a href="#" target="_blank">Twitter</a></li>
+        <li><a href="#" target="_blank">LinkDin</a></li>
+    </ul>
+</div>`
+}
+
+export default footer;
